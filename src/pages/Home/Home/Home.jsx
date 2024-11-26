34a4129dcb17e68eec4702582817e2dac6c283eb
hossainshakil242 <hossainshakil242@gmail.com>
2024-11-26 22:20:55 +0600

    setup basic router and install daisyUI,Tainwind

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Bangla Bazar</h1>
+      <button className="btn btn-secondary">Order Now</button>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+    return (
+        <div>
+            this is nav bar.
+        </div>
+    );
+};
+
+export default Navbar;

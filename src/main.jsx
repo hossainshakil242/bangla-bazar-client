ea690278e40cbdcdf8f4f8761aeeeac5b99e5ed4
hossainshakil242 <hossainshakil242@gmail.com>
2024-11-27 23:51:47 +0600

    firebase authProvider update

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routers/Routers.jsx";
+import AuthPovider from "./providers/AuthPovider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <div className="max-w-screen-xl mx-auto">
-      <RouterProvider router={router}></RouterProvider>
+      <AuthPovider>
+        <RouterProvider router={router}></RouterProvider>
+      </AuthPovider>
     </div>
   </StrictMode>
 );
--- src/providers/AuthPovider.jsx
+++ src/providers/AuthPovider.jsx
@@ -1,10 +1,42 @@
-import React, { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
+
+const AuthContext = createContext(null);
 
 const AuthPovider = ({ children }) => {
-  const AuthContext = createContext(null);
+  const auth = getAuth(app);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  //   create a user
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // login user
+  
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log(`currentUser: ${currentUser}`);
+      setLoading(false);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   const authInfo = {
     name: "shakil",
+    createUser,
+    loading,
   };
 
   return (
